92d23595c499572126cdf816bbd95ef7c5ff7e40
Guilherme <gui.estudo00@gmail.com>
2026-07-21 17:21:08 -0300

    feat: configura conexão do Sequelize com Postgres via Supabase

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,22 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+dotenv.config();
+/* Esse cara é quem puxa o .env e "encaixa" as informações 
+dentro de cada chamada de process.env.[variavel] */
 
-export default sequelize
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+);
+
+export default sequelize;
+
+sequelize.authenticate()
+  .then(() => console.log('Conexão com o banco estabelecida com sucesso!'))
+  .catch((erro) => console.error('Não foi possível conectar ao banco:', erro));

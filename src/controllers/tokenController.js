2a4df91ffd032a137af03594090b83410c566683
Guilherme <gui.estudo00@gmail.com>
2026-09-15 18:13:08 -0300

    feat: inclui dados do usuário e do carro nas respostas de login/create/update

--- src/controllers/tokenController.js
+++ src/controllers/tokenController.js
@@ -1,10 +1,10 @@
-import db from '../models/index.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import db from "../models/index.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export async function loginUser(req, res) {
   const { username, password } = req.body;
-  const msg = 'Usuário ou Senha não enviado';
+  const msg = "Usuário ou Senha não enviado";
 
   try {
     if (!username) return res.status(400).json({ message: msg });
@@ -13,31 +13,38 @@ export async function loginUser(req, res) {
     const match = await db.User.findOne({ where: { username: username } });
 
     if (!match) {
-      return res.status(401).json({ message: 'Credenciais inválidas' });
+      return res.status(401).json({ message: "Credenciais inválidas" });
     } else {
+      const userRes = {
+        id: match.id,
+        name: match.name,
+        username: match.username,
+      };
+
       if (!(await bcrypt.compare(password, match.password))) {
-        return res.status(401).json({ message: 'Credenciais inválidas' });
+        return res.status(401).json({ message: "Credenciais inválidas" });
       } else {
         const token = jwt.sign({ id: match.id }, process.env.JWT_SECRET, {
-          expiresIn: '30m',
+          expiresIn: "30m",
         });
 
-        res.cookie('token', token, {
+        res.cookie("token", token, {
           httpOnly: true,
           maxAge: 1800000,
-          sameSite: 'strict', // Pode mudar no deploy
-          secure: process.env.NODE_ENV === 'production',
+          sameSite: "strict", // Pode mudar no deploy
+          secure: process.env.NODE_ENV === "production",
         });
 
-        return res
-          .status(200)
-          .json({ message: 'Login realizado com sucesso.' });
+        return res.status(200).json({
+          user: userRes,
+          message: "Login realizado com sucesso.",
+        });
       }
     }
   } catch (e) {
     console.log(e);
     res
       .status(500)
-      .json({ message: 'Um erro interno ocorreu, tente novamente.' });
+      .json({ message: "Um erro interno ocorreu, tente novamente." });
   }
 }

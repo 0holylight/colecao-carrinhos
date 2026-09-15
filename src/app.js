858dc74867df2961aead4ff24837e5a428a84c34
Guilherme <gui.estudo00@gmail.com>
2026-09-15 18:24:43 -0300

    feat: serve pasta de uploads publicamente e registra aprovação do script do bcrypt

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import userRoutes from './routes/userRoutes.js';
-import tokenRoutes from './routes/tokenRoutes.js';
-import carRoutes from './routes/carRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
+import tokenRoutes from "./routes/tokenRoutes.js";
+import carRoutes from "./routes/carRoutes.js";
 
-import { errorHandler } from './middlewares/errorHandler.js';
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
-// MIDDLEWARES
+// MIDDLEWARES - INFRAESTRUTURA GENÉRICA
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: "http://localhost:5173",
     credentials: true,
   }),
 );
 app.use(cookieParser());
+app.use("/uploads", express.static("uploads"));
 
-// ROUTES
-app.use('/usuarios', userRoutes);
-app.use('/tokens', tokenRoutes);
-app.use('/carros', carRoutes);
+// ROUTES - LÓGICA DE APLICAÇÃO PRÓPRIA
+app.use("/usuarios", userRoutes);
+app.use("/tokens", tokenRoutes);
+app.use("/carros", carRoutes);
 
 // ERROR-HANDLER
 app.use(errorHandler);

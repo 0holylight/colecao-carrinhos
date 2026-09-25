c2c13fd101803da2061878d176e6c7e0498063c8
Guilherme <gui.estudo00@gmail.com>
2026-09-25 18:41:33 -0300

    chore: sincroniza formatação e imports entre máquinas

--- src/routes/carRoutes.js
+++ src/routes/carRoutes.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import upload from '../config/uploads.js';
+import express from "express";
+import { upload } from "../config/uploads.js";
 
 import {
   createCar,
@@ -7,16 +7,16 @@ import {
   updateCar,
   deleteCar,
   removeCarPhoto,
-} from '../controllers/carController.js';
+} from "../controllers/carController.js";
 
-import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/', authMiddleware, upload.single('photo'), createCar);
-router.get('/', authMiddleware, getCar);
-router.put('/:id', authMiddleware, upload.single('photo'), updateCar);
-router.delete('/:id', authMiddleware, deleteCar);
-router.delete('/:id/photo', authMiddleware, removeCarPhoto);
+router.post("/", authMiddleware, upload.single("photo"), createCar);
+router.get("/", authMiddleware, getCar);
+router.put("/:id", authMiddleware, upload.single("photo"), updateCar);
+router.delete("/:id", authMiddleware, deleteCar);
+router.delete("/:id/photo", authMiddleware, removeCarPhoto);
 
 export default router;
